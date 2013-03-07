63de9b45ceb3edf57247cd7062532fa836b5f56d
Eleanor Kiefel Haggerty <eleanorakh@gmail.com>
2013-03-07 21:29:25 +1000

    Add extra describe.

--- spec/human_spec.rb
+++ spec/human_spec.rb
@@ -1,24 +1,26 @@
 require 'human'
 
 describe 'Human' do
-  before do
-    @human = Human.new('Odin', 60, 5.11, 1)
-  end
+  describe 'with valid attributes' do
+    before do
+      @human = Human.new('Odin', 60, 5.11, 1)
+    end
 
-  it 'has a name' do
-    expect(@human.name).to eq 'Odin'
-  end
+    it 'has a name' do
+      expect(@human.name).to eq 'Odin'
+    end
 
-  it 'has a weight' do
-    expect(@human.weight).to eq 60
-  end
+    it 'has a weight' do
+      expect(@human.weight).to eq 60
+    end
 
-  it 'has a height' do
-    expect(@human.height).to eq 5.11
-  end
+    it 'has a height' do
+      expect(@human.height).to eq 5.11
+    end
 
-  it 'has a fitness level' do
-    expect(@human.fitness_level).to eq 1
+    it 'has a fitness level' do
+      expect(@human.fitness_level).to eq 1
+    end
   end
 end
 
