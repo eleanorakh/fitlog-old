bfaae80ad8355dcb5b37c97fc2a6b4a2b2f75911
Eleanor Kiefel Haggerty <eleanorakh@gmail.com>
2013-03-10 14:15:33 +1000

    Add interpolation comment.

--- spec/points_spec.rb
+++ spec/points_spec.rb
@@ -6,7 +6,7 @@ describe 'Points' do
       human = stub(:name => 'Ron Weasley')
       points = Points.new(human)
       expect(points.total_points).to eq "Total points for #{human.name}: 0"
-      # the #{} is called interpolation
+      # the #{} is called interpolation. interpolation brings ruby into a string.
     end
   end
 end
