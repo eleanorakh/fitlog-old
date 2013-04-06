ad426f3ab68dcda36a0db66dc3bc3f36168e2536
Eleanor Kiefel Haggerty <eleanorakh@gmail.com>
2013-04-06 15:31:42 +1000

    Test viewing a human.

--- spec/fitlog_spec.rb
+++ spec/fitlog_spec.rb
@@ -9,9 +9,16 @@ describe 'fitlog' do
     Sinatra::Application
   end
 
-  it 'returns a name' do
-    get '/name/draco'
+  it 'shows humans' do
+    human = Human.new(:name => 'Draco', :weight => 67, :height => 5.11, :fitness_level => 4)
+    human.save
+
+    get "/human/#{human.id}"
+
     expect(last_response).to be_ok
-    expect(last_response.body).to eq 'Name: draco'
+    expect(last_response.body).to include "Name:\nDraco"
+    expect(last_response.body).to include "Weight:\n67"
+    expect(last_response.body).to include "Height:\n5.11"
+    expect(last_response.body).to include "Fitness Level:\n4"
   end
 end
