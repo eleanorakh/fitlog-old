f3449ee3a03b25e1ba1ef37910626912e23958d0
Eleanor Kiefel Haggerty <eleanorakh@gmail.com>
2013-04-06 14:28:10 +1000

    Clean up Human.

--- human.rb
+++ human.rb
@@ -9,17 +9,4 @@ class Human
 
   validates_presence_of :fitness_level
   validates_within :fitness_level, :set => 1..5
-
-  # adds a method to the object to read the instance variable.
-  attr_reader :name
-  attr_reader :weight
-  attr_reader :height
-  attr_accessor :fitness_level
-
-  def initialize(name, weight, height, fitness_level)
-    @name = name
-    @weight = weight
-    @height = height
-    @fitness_level = fitness_level
-  end
 end
